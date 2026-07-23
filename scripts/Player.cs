51dcc060b18b88bb8fc7b8d12086e25dab23fae8
Matthew Lin <144387789+Neapolicy@users.noreply.github.com>
2026-07-22 23:12:24 -0400

    player fi

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -1,26 +1,35 @@
 using Godot;
 using System;
 
-public partial class Player : Node2D
+public partial class Player : CharacterBody2D
 {
-	public static Player Instance;
-	[Export] private float speed = 10.0f;
-	[Export] private RemoteTransform2D remoteTransform2D_;
-	private Vector2 direction;
-	public override void _Ready()
-	{
-		Instance = this;
-		direction = Vector2.Right;
-	}
+	private int speed = 130;
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+	public override void _PhysicsProcess(double delta)
 	{
-		Vector2 InputMovement = Input.GetVector("move_left", "move_right", "move_up", "move_down");
-		direction = InputMovement;
+		Vector2 direction = Vector2.Zero;
 
-		Vector2 CurrentPosition = Position;
-		CurrentPosition += InputMovement * speed;
-		Position = CurrentPosition;
+		if (Input.IsActionPressed("move_right"))
+		{
+			direction.X += 1;
+		}
+
+		if (Input.IsActionPressed("move_left"))
+		{
+			direction.X -= 1;
+		}
+
+		if (Input.IsActionPressed("move_down"))
+		{
+			direction.Y += 1;
+		}
+
+		if (Input.IsActionPressed("move_up"))
+		{
+			direction.Y -= 1;
+		}
+		GD.Print(Position);
+		Velocity = direction.Normalized() * speed;
+		MoveAndSlide();
 	}
 }
