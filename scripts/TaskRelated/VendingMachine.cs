76b6843317e8e42f5d9f2b005893884dfd55f430
Bob Lin <smallgiant1010@gmail.com>
2026-07-24 01:19:23 -0400

    removed some default comments

--- scripts/TaskRelated/VendingMachine.cs
+++ scripts/TaskRelated/VendingMachine.cs
@@ -4,7 +4,6 @@ using System;
 public partial class VendingMachine : BreakableInteractive
 {
 	private float itemUpcharge = 0.5f;
-	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		base._Ready();
@@ -15,7 +14,6 @@ public partial class VendingMachine : BreakableInteractive
 		
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 		base._Process(delta);
